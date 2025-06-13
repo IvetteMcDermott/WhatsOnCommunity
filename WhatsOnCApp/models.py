7aa5c9523a1eb819986b89e224603e2fbec9e68d
IvetteMcDermott <ivettedemcdermott@gmail.com>
2025-06-13 19:13:27 +0100

    Migration: Modification in DB
    Added field description to provider and event
    Added the boolean field approved, to filter and can show just what admin approved

--- WhatsOnCApp/models.py
+++ WhatsOnCApp/models.py
@@ -4,7 +4,6 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Category(models.Model):
-
     class Meta:
         verbose_name_plural='Categories'
     
@@ -14,24 +13,31 @@ class Category(models.Model):
         return self.name
 
 class Provider(models.Model):
-
     name=models.CharField(max_length=100, blank=False, null=False)
     phone=models.IntegerField(blank=False, null=False)
     email=models.EmailField(null=False, blank=False)
     location=models.CharField(max_length=200, null=False, blank=False)
     vcode=models.CharField(max_length=10, null=False, blank=False)
+    description=models.TextField(max_length=500, null=False, blank=True)
+    approved=models.BooleanField(default=False)
 
 class Image(models.Model):
+    provider= models.ForeignKey(Provider, on_delete=models.CASCADE, related_name="provider_img")
     image=CloudinaryField(max_length=255, verbose_name='image' )
     caption=models.CharField(max_length=200, null=False, blank=False)
 
 class Event(models.Model):
-
     category=models.ForeignKey(Category, on_delete=models.CASCADE, default='Category')
     title=models.CharField(max_length=200, null=False, blank=False)
     date=models.DateField(null=False, blank=False)
     time=models.TimeField(null=False, blank=False)
     location=models.CharField(max_length=200, null=False, blank=False)
     vcode=models.CharField(max_length=10,null=False, blank=False)
+    description=models.TextField(max_length=500, null=False, blank=True)
     provider=models.ForeignKey(Provider, on_delete=models.CASCADE, default='Provider')
+    appproved=models.BooleanField(default=False)
     
+class ImageEvent(models.Model):
+    event= models.ForeignKey(Event, on_delete=models.CASCADE, related_name="provider_img")
+    image=CloudinaryField(max_length=255, verbose_name='image' )
+    caption=models.CharField(max_length=200, null=False, blank=False)
