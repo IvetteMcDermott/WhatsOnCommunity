a9672280f1f94578f1b17e6b4d2d466041568502
IvetteMcDermott <ivettedemcdermott@gmail.com>
2025-06-13 19:14:13 +0100

    Register models for Admin Panel

--- WhatsOnCApp/admin.py
+++ WhatsOnCApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Provider, Image, Category,  Event, ImageEvent
 
-# Register your models here.
+admin.site.register(Provider)
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Event)
+admin.site.register(ImageEvent)
