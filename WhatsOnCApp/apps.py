53399afa08fa9bc56634720e6faad4ac10b64005
IvetteMcDermott <ivettedemcdermott@gmail.com>
2025-07-11 21:22:05 +0100

    Create profile for user when register by signals.py

--- WhatsOnCApp/apps.py
+++ WhatsOnCApp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class WhatsoncappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'WhatsOnCApp'
+
+    def ready(self):
+        import WhatsOnCApp.signals
