83b06ecde0bc13b5ebb45961e2dfee247f2b152e
IvetteMcDermott <ivettedemcdermott@gmail.com>
2025-07-11 21:28:34 +0100

    Boorkmark functionality

--- CommunityApp/urls.py
+++ CommunityApp/urls.py
@@ -5,4 +5,5 @@ app_name="CommunityApp"
 
 urlpatterns=[path('userProfile/', views.userProfile, name="userProfile"),
              path('providerProfile/', views.providerProfile, name="providerProfile"),
+             path('bookmark/<int:id>', views.bookmarking, name='bookmark')
 ]
--- CommunityApp/views.py
+++ CommunityApp/views.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from .models import UserProfile, Bookmarks
+from WhatsOnCApp.models import Event
+from django.contrib.auth.models import User
+
+
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
-from django.shortcuts import render
-
-
-# Create your views here.
+@login_required
 def userProfile(request):
+
     return render(request, 'userProfile.html')
 
 def providerProfile(request):
     return render(request, 'providerProfile.html')
+
+@login_required
+def bookmarking(request, id):
+    if request.method=='POST':
+        user = UserProfile.objects.get(user=request.user)
+        event=get_object_or_404(Event, id=id)
+        bookmark_event, created = Bookmarks.objects.get_or_create(user=user, event=event)
+             
+        if created:
+            # If created, bookmark didn't exist
+            print("Bookmark added.")
+        else:
+            # If existed, delete it
+            bookmark_event.delete()
+            print("Bookmark removed.")
+
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
