74ad5f7a1611230fc20a42c207f7eb1b301fdf41
IvetteMcDermott <ivettedemcdermott@gmail.com>
2025-07-23 22:08:44 +0100

    Change of capitalized folders

--- whatsoncommunityproject/settings.py
+++ whatsoncommunityproject/settings.py
@@ -78,7 +78,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [TEMPLATES_DIR,
-                 os.path.join(BASE_DIR, 'Templates', 'allauth'),
+                 os.path.join(BASE_DIR, 'templates', 'allauth'),
                  ],
         'APP_DIRS': True,
         'OPTIONS': {
